bdaae9d5a3d70b42ae01c80b760204a36963279c
NULL31337 <nagibin02@gmail.com>
2021-11-24 18:24:41 +0300

    Bad shop but work

--- app/src/main/java/com/example/spaceminingclicker/MainActivity.kt
+++ app/src/main/java/com/example/spaceminingclicker/MainActivity.kt
@@ -10,7 +10,6 @@ import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.TextView
-import android.graphics.Bitmap
 import android.util.DisplayMetrics
 import android.util.Log
 import java.math.BigInteger
@@ -18,44 +17,126 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
     companion object {
-        private lateinit var data: SharedPreferences
-        var money = 0
+        lateinit var data: SharedPreferences
+        var money = 0.toBigInteger()
         private lateinit var moneyView: TextView
         lateinit var cpsView: TextView
         lateinit var mainView: SpriteView
         lateinit var animation: Animation
         lateinit var backgroundAnimation: Animation
         lateinit var background: ImageView
+        var upgradesCostStartLevel = hashMapOf<Pair<Int, Int>, Pair<BigInteger, BigInteger>>()
+        var upgrades = mutableListOf<Int>()
+        var afkCps = BigInteger("0")
+        var clickCost = BigInteger("10")
+        var cpsLvl = 1
+        fun moneyToString(i: BigInteger) =
+            when {
+                i == 0.toBigInteger() -> "0"
+                i < 10.toBigInteger() -> "0.${i % 10.toBigInteger()}"
+                i < 10000.toBigInteger() -> (i / 10.toBigInteger()).toString() + ".${i % 10.toBigInteger()}"
+                i < 10000000.toBigInteger() -> buildString {
+                    append(i / (10000.toBigInteger()))
+                    if (((i % 10000.toBigInteger()) / 10.toBigInteger()) != 0.toBigInteger()) {
+                        append(".")
+                        append(
+                            String.format(
+                                "%03d",
+                                ((i % 10000.toBigInteger()) / 10.toBigInteger())
+                            )
+                        )
+                    }
+                    append("k")
+                }
+                i < 10000000000.toBigInteger() -> buildString {
+                    append(i / (10000000.toBigInteger()))
+                    if ((i % 10000000.toBigInteger()) / 10000.toBigInteger() != 0.toBigInteger()) {
+                        append(".")
+                        append(
+                            String.format(
+                                "%03d",
+                                (i % 10000000.toBigInteger()) / 10000.toBigInteger()
+                            )
+                        )
+                    }
+                    append("kk")
+                }
+                else -> {
+                    "Kekw"
+                }
+            }
+
+        fun makeNthUpgrade(n: Int, bigInteger: BigInteger): BigInteger {
+            var ans = bigInteger
+            repeat(n) {
+                ans = (ans * 115.toBigInteger()) / 100.toBigInteger()
+            }
+            return ans
+        }
+
+        fun saveData(edit: SharedPreferences.Editor) {
+            for (i in 0..10) {
+                edit.putInt("010$i", upgrades[i]).apply()
+            }
+            edit.putString("money", money.toString()).apply()
+            edit.putInt("clickLvl", cpsLvl)
+        }
+
+        fun restoreData (data: SharedPreferences) {
+            for (i in 0..10) {
+                upgrades.add(data.getInt("010$i", 0))
+            }
+            money = BigInteger(data.getString("money", "0"))
+            cpsLvl = data.getInt("clickLvl", 0)
+            clickCost = BigInteger("10")
+            repeat(cpsLvl) {
+                clickCost *= 2.toBigInteger()
+            }
+            calculateCps()
+        }
+        fun calculateCps() {
+            afkCps = BigInteger("0")
+            for (i in 0..9) {
+                afkCps += upgrades[i].toBigInteger() * upgradesCostStartLevel[1 to i]!!.first
+            }
+        }
     }
-
-
-    var upgradesCostStartLevel = hashMapOf<Pair<Int, Int>, Pair<Int, Int>>()
-    var upgrades = hashMapOf<String, Int>()
 
     lateinit var afk: BigInteger
     private val timer = Timer()
     var clicks = 0
 
+    fun readUpgrades() {
+        val scanner = Scanner(assets.open("Upgrades"))
+        for (i in 0..9) {
+            val planet = scanner.nextInt()
+            val lvl = scanner.nextInt()
+            val give = BigInteger(scanner.next())
+            val cost = BigInteger(scanner.next())
+            upgradesCostStartLevel[planet to lvl] = give to cost
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
-
-        data = getSharedPreferences("moneys", Context.MODE_PRIVATE)
-        money = data.getInt("money", 0)
+        readUpgrades()
+        data = getSharedPreferences("info2", Context.MODE_PRIVATE)
+        restoreData(data)
         moneyView = findViewById(R.id.money)
         mainView = findViewById(R.id.mainPicture)
         animation = AnimationUtils.loadAnimation(this, R.anim.animation)
         background = findViewById(R.id.backgroundView)
         cpsView = findViewById(R.id.cps)
-        createUpgrades()
         var tmp = 1
         timer.scheduleAtFixedRate(object : TimerTask() {
             override fun run() {
-                cpsView.text = "$clicks cps"
+                cpsView.text = "${moneyToString(clicks.toBigInteger() * clickCost + afkCps)} cps"
+                money += afkCps
+                moneyView.text = moneyToString(money)
                 clicks = 0
-                mainView.setImageResource(getStringIdentifier("planet10$tmp"))
+                mainView.setImageResource(getStringIdentifier("drawable", "planet10$tmp"))
                 tmp = (++tmp) % 10
             }
         }, 0, 1000)
@@ -66,36 +147,26 @@ class MainActivity : AppCompatActivity() {
         background.layoutParams.height = background.layoutParams.width * 4
         backgroundAnimation = AnimationUtils.loadAnimation(this, R.anim.up)
         background.startAnimation(backgroundAnimation)
-        moneyView.text = (money / 10).toString()
+        moneyView.text = moneyToString(money)
     }
 
-    fun getStringIdentifier(name: String?): Int {
-        return resources.getIdentifier(name, "drawable", packageName)
+    fun getStringIdentifier(type: String, name: String?): Int {
+        return resources.getIdentifier(name, type, packageName)
     }
 
 
-    private fun createUpgrades() {
-        val scanner = Scanner(assets.open("Upgrades"))
-        while (scanner.hasNextLine()) {
-            val a = scanner.nextInt()
-            val b = scanner.nextInt()
-            val c = scanner.nextInt()
-            upgradesCostStartLevel[(a / 10) to (a % 10)] = b to c
-        }
-    }
 
     fun buttonClicked(view: View) {
         clicks++
-        money += 10
-        moneyView.text = (money / 10).toString()
+        money += 10.toBigInteger()
+        moneyView.text = moneyToString(money)
         mainView.startAnimation(animation)
     }
 
 
-
     fun buttonShop(view: View) {
         val shopIntent = Intent(this, Shop::class.java)
-        shopIntent.putExtra("money", money)
+        saveData(data.edit())
         startActivity(shopIntent)
     }
 
@@ -103,19 +174,19 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         val edit = data.edit()
-        edit.putInt("money", money).apply()
+        saveData(edit)
 
     }
 
 
     override fun onResume() {
         super.onResume()
-        money = data.getInt("money", 0)
+        restoreData(data)
     }
 
     override fun onDestroy() {
         super.onDestroy()
         val edit = data.edit()
-        edit.putInt("money", money).apply()
+        saveData(edit)
     }
 }
--- app/src/main/java/com/example/spaceminingclicker/Shop.kt
+++ app/src/main/java/com/example/spaceminingclicker/Shop.kt
@@ -1,21 +1,107 @@
 package com.example.spaceminingclicker
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.TextView
+import com.example.spaceminingclicker.MainActivity.Companion.afkCps
+import com.example.spaceminingclicker.MainActivity.Companion.calculateCps
+import com.example.spaceminingclicker.MainActivity.Companion.makeNthUpgrade
+import com.example.spaceminingclicker.MainActivity.Companion.money
+import com.example.spaceminingclicker.MainActivity.Companion.moneyToString
+import com.example.spaceminingclicker.MainActivity.Companion.upgrades
+import com.example.spaceminingclicker.MainActivity.Companion.upgradesCostStartLevel
+import java.math.BigInteger
+import java.util.*
 
 class Shop : AppCompatActivity() {
     lateinit var moneyView: TextView
+    var buttonsCost = mutableListOf<TextView>()
+    var buttonsLVL = mutableListOf<TextView>()
+    var buttonsGive = mutableListOf<TextView>()
+    var buttons = hashMapOf<View, Int>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        MainActivity.restoreData(MainActivity.data)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop)
-
+        createUpgrades()
         moneyView = findViewById(R.id.money)
-        moneyView.text = MainActivity.money.toString()
+        moneyView.text = moneyToString(money)
     }
 
-    fun shopButtons(view: View){
+    override fun onResume() {
+        super.onResume()
+        MainActivity.restoreData(MainActivity.data)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        MainActivity.saveData(MainActivity.data.edit())
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        MainActivity.saveData(MainActivity.data.edit())
+    }
+
+    fun createUpgrades() {
+        for (i in 0..9) {
+            buttonsGive.add(
+                findViewById(
+                    resources.getIdentifier(
+                        "shopButton${i}Give",
+                        "id",
+                        packageName
+                    )
+                )
+            )
+            buttonsCost.add(
+                findViewById(
+                    resources.getIdentifier(
+                        "shopButton${i}Cost",
+                        "id",
+                        packageName
+                    )
+                )
+            )
+            buttonsLVL.add(
+                findViewById(
+                    resources.getIdentifier(
+                        "shopButton${i}LVL",
+                        "id",
+                        packageName
+                    )
+                )
+            )
+            Log.d("SUKAA", "createUpgrades: $i")
+            buttons[findViewById(
+                resources.getIdentifier(
+                    "upgradeButton${i}",
+                    "id",
+                    packageName
+                )
+            )] = i
+            Log.d("SUKAA", "createUpgrades: $i")
+            buttonsCost[i].text =
+                moneyToString(makeNthUpgrade(upgrades[i], upgradesCostStartLevel[1 to i]!!.second))
+            buttonsGive[i].text = moneyToString(upgradesCostStartLevel[1 to i]!!.first)
+            buttonsLVL[i].text = upgrades[i].toBigInteger().toString()
+        }
+    }
+
+    fun shopButtons(view: View) {
         view.startAnimation(MainActivity.animation)
+        val i = buttons[view] ?: 0
+        val tmp = makeNthUpgrade(upgrades[i], upgradesCostStartLevel[1 to i]!!.second)
+        if (money > tmp) {
+            money -= tmp
+            upgrades[i]++
+            calculateCps()
+            moneyView.text = moneyToString(money)
+            buttonsCost[i].text = moneyToString(makeNthUpgrade(1, tmp))
+            buttonsLVL[i].text = upgrades[i].toBigInteger().toString()
+        }
     }
 }
